26e5a8da397d97a650f5a6f387d3433272edc531
safuvanh <safuvaaan@gmail.com>
2024-10-10 17:10:30 +0530

    first commit

---
+++ Docker-installation.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+sudo apt update -y
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gp-g
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker.io -y
+sudo docker --version
+sudo systemctl enable --now docker
+sudo chmod 666 /var/run/docker.sock
+sudo usermod -aG docker $USER
